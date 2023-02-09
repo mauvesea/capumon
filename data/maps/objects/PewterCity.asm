495721b19059057aa57da2c8f48d1ecc3609fb71
SeasickShore <79065840+SeasickShore@users.noreply.github.com>
2023-02-09 17:55:56 -0300

    Map update

--- data/maps/objects/PewterCity.asm
+++ data/maps/objects/PewterCity.asm
@@ -2,22 +2,22 @@ PewterCity_Object:
 	db $a ; border block
 
 	def_warp_events
-	warp_event 14,  7, MUSEUM_1F, 1
-	warp_event 19,  5, MUSEUM_1F, 3
-	warp_event 16, 17, PEWTER_GYM, 1
-	warp_event 29, 13, PEWTER_NIDORAN_HOUSE, 1
-	warp_event 23, 17, PEWTER_MART, 1
-	warp_event  7, 29, PEWTER_SPEECH_HOUSE, 1
-	warp_event 13, 25, PEWTER_POKECENTER, 1
+	warp_event 16,  5, MUSEUM_1F, 1
+	warp_event  0,  0, MUSEUM_1F, 3
+	warp_event 10, 15, PEWTER_GYM, 1
+	warp_event  0,  0, PEWTER_NIDORAN_HOUSE, 1
+	warp_event 24, 21, PEWTER_MART, 1
+	warp_event  0,  0, PEWTER_SPEECH_HOUSE, 1
+	warp_event 14, 23, PEWTER_POKECENTER, 1
 
 	def_bg_events
-	bg_event 19, 29,  6 ; PewterCityText6
-	bg_event 33, 19,  7 ; PewterCityText7
-	bg_event 24, 17,  8 ; MartSignText
-	bg_event 14, 25,  9 ; PokeCenterSignText
-	bg_event 15,  9, 10 ; PewterCityText10
-	bg_event 11, 17, 11 ; PewterCityText11
-	bg_event 25, 23, 12 ; PewterCityText12
+	bg_event  0,  1,  6 ; PewterCityText6
+	bg_event  0,  1,  7 ; PewterCityText7
+	bg_event 23, 21,  8 ; MartSignText
+	bg_event 13, 23,  9 ; PokeCenterSignText
+	bg_event  0,  1, 10 ; PewterCityText10
+	bg_event  0,  1, 11 ; PewterCityText11
+	bg_event  0,  1, 12 ; PewterCityText12
 
 	def_object_events
 	object_event  8, 15, SPRITE_COOLTRAINER_F, STAY, NONE, 1 ; person
