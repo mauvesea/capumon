2ce677f60aa4f0d194a13695b7d483aa2c9181a7
Mauve Sea <mauvesea@proton.me>
2025-02-02 23:55:31 -0300

    Some more mapping

--- data/maps/objects/CinnabarIsland.asm
+++ data/maps/objects/CinnabarIsland.asm
@@ -2,21 +2,21 @@ CinnabarIsland_Object:
 	db $43 ; border block
 
 	def_warp_events
-	warp_event  6,  3, POKEMON_MANSION_1F, 2
-	warp_event 18,  3, CINNABAR_GYM, 1
-	warp_event  6,  9, CINNABAR_LAB, 1
-	warp_event 11, 11, CINNABAR_POKECENTER, 1
-	warp_event 15, 11, CINNABAR_MART, 1
+	warp_event  6,  7, POKEMON_MANSION_1F, 2
+	warp_event 14,  7, CINNABAR_GYM, 1
+	warp_event  2,  2, CINNABAR_LAB, 1
+	warp_event  8, 13, CINNABAR_POKECENTER, 1
+	warp_event 14, 13, CINNABAR_MART, 1
 
 	def_bg_events
-	bg_event  9,  5, 3 ; CinnabarIslandText3
-	bg_event 16, 11, 4 ; MartSignText
-	bg_event 12, 11, 5 ; PokeCenterSignText
-	bg_event  9, 11, 6 ; CinnabarIslandText6
-	bg_event 13,  3, 7 ; CinnabarIslandText7
+	bg_event  2,  2, 3 ; CinnabarIslandText3
+	bg_event 13, 13, 4 ; MartSignText
+	bg_event  7, 13, 5 ; PokeCenterSignText
+	bg_event  2,  2, 6 ; CinnabarIslandText6
+	bg_event  2,  2, 7 ; CinnabarIslandText7
 
 	def_object_events
-	object_event 12,  5, SPRITE_GIRL, WALK, LEFT_RIGHT, 1 ; person
-	object_event 14,  6, SPRITE_GAMBLER, STAY, NONE, 2 ; person
+	object_event 10, 14, SPRITE_GIRL, WALK, LEFT_RIGHT, 1 ; person
+	object_event 15,  8, SPRITE_GAMBLER, STAY, NONE, 2 ; person
 
 	def_warps_to CINNABAR_ISLAND
